c2b75dff151d2e0bf5b91fea15d627866547d6f8
Abdul Kalam Shekh <37735290+abdul97233@users.noreply.github.com>
2024-08-11 12:10:02 +0545

    Update levi.py

--- levi.py
+++ levi.py
@@ -94,8 +94,18 @@ class Bot(Client):
             sys.exit()
 
         self.set_parse_mode(ParseMode.HTML)
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/ntmchat")
-        self.LOGGER(__name__).info(f"Developer:- @abdul97233")
+        # self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/ntmchat")
+        # self.LOGGER(__name__).info(f"Developer:- @abdul97233")
+        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/NTMPRo")
+        self.LOGGER(__name__).info(f""" \n\n       
+ooooo      ooo ooooooooooooo ooo        ooooo 
+`888b.     `8' 8'   888   `8 `88.       .888' 
+ 8 `88b.    8       888       888b     d'888  
+ 8   `88b.  8       888       8 Y88. .P  888  
+ 8     `88b.8       888       8  `888'   888  
+ 8       `888       888       8    Y     888  
+o8o        `8      o888o     o8o        o888o 
+                                          """)
         self.username = usr_bot_me.username
         #web-response
         app = web.AppRunner(await web_server())
